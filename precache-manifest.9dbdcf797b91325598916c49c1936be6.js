38892d1831fd095115b324d021ea08dfadc4fcd2
Tobin Nelson <tobin.j.n@gmail.com>
2018-11-11 09:23:09 -0700

    Updates

---
+++ precache-manifest.9dbdcf797b91325598916c49c1936be6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69c543f0dcd5a3a7093e37c012a145c5",
+    "url": "/Jedi-Archives-Access/static/media/galbasic.69c543f0.ttf"
+  },
+  {
+    "revision": "27a65a57e2a9a86b14ef",
+    "url": "/Jedi-Archives-Access/static/js/runtime~main.27a65a57.js"
+  },
+  {
+    "revision": "c7fb23d40d4e1ba9b04a",
+    "url": "/Jedi-Archives-Access/static/js/main.c7fb23d4.chunk.js"
+  },
+  {
+    "revision": "67122806e036f006282e",
+    "url": "/Jedi-Archives-Access/static/js/1.67122806.chunk.js"
+  },
+  {
+    "revision": "c7fb23d40d4e1ba9b04a",
+    "url": "/Jedi-Archives-Access/static/css/main.90e6e118.chunk.css"
+  },
+  {
+    "revision": "185d7fa57f3993a34ebb705adb0ed5c3",
+    "url": "/Jedi-Archives-Access/index.html"
+  }
+];
